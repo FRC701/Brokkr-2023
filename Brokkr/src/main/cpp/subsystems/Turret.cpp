ff94fb6eab4702b59e5cf015de3b0edb1383cb0e
Jesse Melendres <jessejacobm@vandenrobotics.com>
2023-01-16 11:30:08 -0800

    PhotonVision Camera Intergration
    Added functions that retrieve needed info from Limelight

--- Brokkr/src/main/cpp/subsystems/Turret.cpp
+++ Brokkr/src/main/cpp/subsystems/Turret.cpp
@@ -3,8 +3,55 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #include "subsystems/Turret.h"
+#include <frc/smartdashboard/SmartDashboard.h>
 
-Turret::Turret() = default;
+Turret::Turret(WPI_TalonFX& mTurret)
+:TurretMotor(mTurret)
+{
+}
 
 // This method will be called once per scheduler run
-void Turret::Periodic() {}
+void Turret::Periodic() {
+    frc::SmartDashboard::PutBoolean("HasTargets?", HasTargets());
+    frc::SmartDashboard::PutNumber("GetYawVision", GetVisionYaw());
+    frc::SmartDashboard::PutNumber("GetPitchVision", GetVisionPitch());
+    frc::SmartDashboard::PutNumber("GetAreaision", GetVisionArea());
+    frc::SmartDashboard::PutNumber("GetVision", camera.GetPipelineIndex());
+}
+
+double Turret::GetTurretPose(){
+    return TurretMotor.GetSelectedSensorPosition();
+}
+
+double Turret::GetVisionArea(){
+     photonlib::PhotonPipelineResult result = camera.GetLatestResult();
+        return result.GetBestTarget().GetArea();
+}
+
+double Turret::GetVisionYaw(){
+    photonlib::PhotonPipelineResult result = camera.GetLatestResult();
+        return result.GetBestTarget().GetYaw();
+}
+
+double Turret::GetVisionPitch(){
+    photonlib::PhotonPipelineResult result = camera.GetLatestResult();
+        return result.GetBestTarget().GetPitch();
+}
+bool Turret::HasTargets(){
+    photonlib::PhotonPipelineResult result = camera.GetLatestResult();
+        return result.HasTargets();
+}
+
+void Turret::SetVisionLED(photonlib::LEDMode index){
+    camera.SetLEDMode(index);
+}
+
+int Turret::SetPipeline(int pipeIndex){
+    camera.SetPipelineIndex(pipeIndex);
+        return pipeIndex;
+}
+
+double Turret::SetTurretPose(double pose){
+    TurretMotor.Set(pose);
+        return pose;
+}
