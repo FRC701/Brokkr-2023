4fb0b1432cce28892e01584034b773247207e6ff
Jesse Melendres <jessejacobm@vandenrobotics.com>
2023-01-19 17:03:59 -0800

    Joystick and Xbox Controller
    Added buttons for driver (joystick) and codriver (Xbox controller)

--- Brokkr/src/main/include/RobotContainer.h
+++ Brokkr/src/main/include/RobotContainer.h
@@ -6,6 +6,9 @@
 
 #include <frc2/command/CommandPtr.h>
 #include <frc2/command/button/CommandXboxController.h>
+#include <frc2/command/button/JoystickButton.h>
+#include <frc/Joystick.h>
+#include <frc2/command/button/Button.h>
 
 #include "Constants.h"
 #include "subsystems/ExampleSubsystem.h"
@@ -46,6 +49,9 @@ class RobotContainer {
   frc2::CommandPtr GetAutonomousCommand();
 
  private:
+ frc::Joystick driver{0};
+ frc::XboxController coDriver{1};
+
   // Replace with CommandPS4Controller or CommandJoystick if needed
   frc2::CommandXboxController m_driverController{
       OperatorConstants::kDriverControllerPort};
