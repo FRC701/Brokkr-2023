de04e67e89d04b0f1b1b8db9b980637f4cc53dc9
Jesse Melendres <jessejacobm@vandenrobotics.com>
2023-01-19 16:27:45 -0800

    ArmPosition Command & SmartDashboard Buttons
    Created a new command and made 4 buttons for smartdashboard.

--- Brokkr/src/main/cpp/RobotContainer.cpp
+++ Brokkr/src/main/cpp/RobotContainer.cpp
@@ -5,20 +5,24 @@
 #include "RobotContainer.h"
 
 #include <frc2/command/button/Trigger.h>
+#include <frc/smartdashboard/SmartDashboard.h>
 
 #include "commands/Autos.h"
 #include "commands/ExampleCommand.h"
+#include "commands/ArmPosition.h"
 
 RobotContainer::RobotContainer() {
   // Initialize all of your commands and subsystems here
-
+ frc::SmartDashboard::PutData("ArmPosition Hybrid", new ArmPosition(mArm, 0)); //Placeholder values
+ frc::SmartDashboard::PutData("ArmPosition Mid", new ArmPosition(mArm, 0));
+ frc::SmartDashboard::PutData("ArmPosition High", new ArmPosition(mArm, 0));
+ frc::SmartDashboard::PutData("ArmPosition Shelf", new ArmPosition(mArm, 0));
   // Configure the button bindings
   ConfigureBindings();
 }
 
 void RobotContainer::ConfigureBindings() {
   // Configure your trigger bindings here
-
   // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
   frc2::Trigger([this] {
     return m_subsystem.ExampleCondition();
