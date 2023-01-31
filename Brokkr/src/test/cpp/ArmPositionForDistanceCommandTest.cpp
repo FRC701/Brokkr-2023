314202d1ac29a64ee6c28941109c0acb6047ae31
Mr.Murphy <dmurphy@vandenrobotics.com>
2023-01-30 20:22:47 -0800

    Actual test for SetArmPositionForDistance
    Actually call the methods of the SetArmPositionForDistance command. 
    
    Good place to start, but not really testing.

--- Brokkr/src/test/cpp/ArmPositionForDistanceCommandTest.cpp
+++ Brokkr/src/test/cpp/ArmPositionForDistanceCommandTest.cpp
@@ -4,6 +4,27 @@
 
 #include "ArmPositionForDistanceCommandTest.h"
 
-TEST_F(ArmPositionForDistanceCommandTest, fake1) {
-    EXPECT_EQ(true, false);
+/*
+    Unit testing uses the AAA pattern for constructing tests:
+    1. Arrange. Setup the test. What are the initial conditions for the test?
+    2. Action. Do something to change the conditions. 
+    3. Assert. Did the change occur the way you expected the change to happen.
+
+    Each of these steps can be simple or complex. Simple is better. Using functions 
+    for arranging is common as many tests will have similar initial conditions.
+*/
+
+TEST_F(ArmPositionForDistanceCommandTest, execute_once) {
+    // Arrange
+    // Initialize the command
+    mCommand.Initialize();
+
+    // Action
+    // Execute the command once
+    mCommand.Execute();
+
+    // Assert
+    // The command should not be done
+    bool isFinished = mCommand.IsFinished();
+    EXPECT_FALSE(isFinished);
 }
