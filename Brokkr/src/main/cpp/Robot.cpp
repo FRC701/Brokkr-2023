dafade3ec94b50d48ca12cfb244cefaec9875432
John Franco Saraceno <OneGneissGuy@users.noreply.github.com>
2023-02-04 11:41:24 -0800

    working elevator sim

--- Brokkr/src/main/cpp/Robot.cpp
+++ Brokkr/src/main/cpp/Robot.cpp
@@ -2,76 +2,130 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-#include "Robot.h"
+#include <numbers>
 
-#include <frc2/command/CommandScheduler.h>
-
-void Robot::RobotInit() {}
-
-/**
- * This function is called every 20 ms, no matter the mode. Use
- * this for items like diagnostics that you want to run during disabled,
- * autonomous, teleoperated and test.
- *
- * <p> This runs after the mode specific periodic functions, but before
- * LiveWindow and SmartDashboard integrated updating.
- */
-void Robot::RobotPeriodic() {
-  frc2::CommandScheduler::GetInstance().Run();
-}
-
-/**
- * This function is called once each time the robot enters Disabled mode. You
- * can use it to reset any subsystem information you want to clear when the
- * robot is disabled.
- */
-void Robot::DisabledInit() {}
-
-void Robot::DisabledPeriodic() {}
+#include <frc/Encoder.h>
+#include <frc/Joystick.h>
+#include <frc/RobotController.h>
+#include <frc/StateSpaceUtil.h>
+#include <frc/TimedRobot.h>
+#include <frc/controller/PIDController.h>
+#include <frc/motorcontrol/PWMSparkMax.h>
+#include <frc/simulation/BatterySim.h>
+#include <frc/simulation/ElevatorSim.h>
+#include <frc/simulation/EncoderSim.h>
+#include <frc/simulation/RoboRioSim.h>
+#include <frc/smartdashboard/Mechanism2d.h>
+#include <frc/smartdashboard/MechanismLigament2d.h>
+#include <frc/smartdashboard/MechanismRoot2d.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <frc/system/plant/LinearSystemId.h>
+#include <frc/util/Color.h>
+#include <frc/util/Color8Bit.h>
+#include <units/angle.h>
+#include <units/length.h>
+#include <units/moment_of_inertia.h>
 
 /**
- * This autonomous runs the autonomous command selected by your {@link
- * RobotContainer} class.
+ * This is a sample program to demonstrate how to use a state-space controller
+ * to control an arm.
  */
-void Robot::AutonomousInit() {
-  m_autonomousCommand = m_container.GetAutonomousCommand();
+class Robot : public frc::TimedRobot {
+  static constexpr int kMotorPort = 0;
+  static constexpr int kEncoderAChannel = 0;
+  static constexpr int kEncoderBChannel = 1;
+  static constexpr int kJoystickPort = 0;
 
-  if (m_autonomousCommand) {
-    m_autonomousCommand->Schedule();
+  static constexpr double kElevatorKp = 5.0;
+  static constexpr double kElevatorGearing = 10.0;
+  static constexpr units::meter_t kElevatorDrumRadius = 2_in;
+  static constexpr units::kilogram_t kCarriageMass = 4.0_kg;
+
+  static constexpr units::meter_t kMinElevatorHeight = 2_in;
+  static constexpr units::meter_t kMaxElevatorHeight = 50_in;
+
+  // distance per pulse = (distance per revolution) / (pulses per revolution)
+  //  = (Pi * D) / ppr
+  static constexpr double kArmEncoderDistPerPulse =
+      2.0 * std::numbers::pi * kElevatorDrumRadius.value() / 4096.0;
+
+  // This gearbox represents a gearbox containing 4 Vex 775pro motors.
+  frc::DCMotor m_elevatorGearbox = frc::DCMotor::Vex775Pro(4);
+
+  // Standard classes for controlling our elevator
+  frc2::PIDController m_controller{kElevatorKp, 0, 0};
+  frc::Encoder m_encoder{kEncoderAChannel, kEncoderBChannel};
+  frc::PWMSparkMax m_motor{kMotorPort};
+  frc::Joystick m_joystick{kJoystickPort};
+
+  // Simulation classes help us simulate what's going on, including gravity.
+  frc::sim::ElevatorSim m_elevatorSim{m_elevatorGearbox,
+                                      kElevatorGearing,
+                                      kCarriageMass,
+                                      kElevatorDrumRadius,
+                                      kMinElevatorHeight,
+                                      kMaxElevatorHeight,
+                                      true,
+                                      {0.01}};
+  frc::sim::EncoderSim m_encoderSim{m_encoder};
+
+  // Create a Mechanism2d display of an elevator
+  frc::Mechanism2d m_mech2d{20, 50};
+  frc::MechanismRoot2d* m_elevatorRoot =
+      m_mech2d.GetRoot("Elevator Root", 10, 0);
+  frc::MechanismLigament2d* m_elevatorMech2d =
+      m_elevatorRoot->Append<frc::MechanismLigament2d>(
+          "Elevator", units::inch_t{m_elevatorSim.GetPosition()}.value(),
+          90_deg);
+
+ public:
+  void RobotInit() override {
+    m_encoder.SetDistancePerPulse(kArmEncoderDistPerPulse);
+
+    // Put Mechanism 2d to SmartDashboard
+    // To view the Elevator Sim in the simulator, select Network Tables ->
+    // SmartDashboard -> Elevator Sim
+    frc::SmartDashboard::PutData("Elevator Sim", &m_mech2d);
   }
-}
 
-void Robot::AutonomousPeriodic() {}
+  void SimulationPeriodic() override {
+    // In this method, we update our simulation of what our elevator is doing
+    // First, we set our "inputs" (voltages)
+    m_elevatorSim.SetInput(frc::Vectord<1>{
+        m_motor.Get() * frc::RobotController::GetInputVoltage()});
 
-void Robot::TeleopInit() {
-  // This makes sure that the autonomous stops running when
-  // teleop starts running. If you want the autonomous to
-  // continue until interrupted by another command, remove
-  // this line or comment it out.
-  if (m_autonomousCommand) {
-    m_autonomousCommand->Cancel();
+    // Next, we update it. The standard loop time is 20ms.
+    m_elevatorSim.Update(20_ms);
+
+    // Finally, we set our simulated encoder's readings and simulated battery
+    // voltage
+    m_encoderSim.SetDistance(m_elevatorSim.GetPosition().value());
+    // SimBattery estimates loaded battery voltages
+    frc::sim::RoboRioSim::SetVInVoltage(
+        frc::sim::BatterySim::Calculate({m_elevatorSim.GetCurrentDraw()}));
+
+    // Update the Elevator length based on the simulated elevator height
+    m_elevatorMech2d->SetLength(
+        units::inch_t{m_elevatorSim.GetPosition()}.value());
   }
-}
 
-/**
- * This function is called periodically during operator control.
- */
-void Robot::TeleopPeriodic() {}
+  void TeleopPeriodic() override {
+    if (m_joystick.GetTrigger()) {
+      // Here, we run PID control like normal, with a constant setpoint of 30in.
+      double pidOutput = m_controller.Calculate(m_encoder.GetDistance(),
+                                                units::meter_t{30_in}.value());
+      m_motor.SetVoltage(units::volt_t{pidOutput});
+    } else {
+      // Otherwise, we disable the motor.
+      m_motor.Set(0.0);
+    }
+  }
 
-/**
- * This function is called periodically during test mode.
- */
-void Robot::TestPeriodic() {}
-
-/**
- * This function is called once when the robot is first started up.
- */
-void Robot::SimulationInit() {}
-
-/**
- * This function is called periodically whilst in simulation.
- */
-void Robot::SimulationPeriodic() {}
+  void DisabledInit() override {
+    // This just makes sure that our simulation code knows that the motor's off.
+    m_motor.Set(0.0);
+  }
+};
 
 #ifndef RUNNING_FRC_TESTS
 int main() {
